25620088fbe99da6a3f1d48b9a92b051f578f846
Ayupov Bulat <b.aiupov@yandex.ru>
2023-10-19 15:35:01 +0500

    6 лаба готовая

--- AyupovAutoService/ServicePage.xaml.cs
+++ AyupovAutoService/ServicePage.xaml.cs
@@ -73,7 +73,7 @@ namespace AyupovAutoService
         }
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            Manager.MainFrame.Navigate(new AddEditPage());
+            Manager.MainFrame.Navigate(new AddEditPage(null));
         }
 
         private void ComboType_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -104,6 +104,24 @@ namespace AyupovAutoService
         private void TBoxSearch_TextChanged(object sender, TextChangedEventArgs e)
         {
             UpdateServices();
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new AddEditPage(null));
+        }
+        private void EditButton_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new AddEditPage((sender as Button).DataContext as Service));
+        }
+
+        private void Page_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if(Visibility == Visibility.Visible) 
+            {
+                Ayupov_ServiceEntities.GetContext().ChangeTracker.Entries().ToList().ForEach(p=>p.Reload());
+                ServiceListView.ItemsSource = Ayupov_ServiceEntities.GetContext().Service.ToList();
+            }
         }
     }
 }
