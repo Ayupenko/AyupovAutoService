098e684f8b50e3727a1a72504eaf8388b61be601
Ayupov Bulat <b.aiupov@yandex.ru>
2023-10-20 16:32:06 +0500

    7 ЛАБА

--- AyupovAutoService/ServicePage.xaml.cs
+++ AyupovAutoService/ServicePage.xaml.cs
@@ -18,8 +18,14 @@ namespace AyupovAutoService
     /// <summary>
     /// Логика взаимодействия для ServicePage.xaml
     /// </summary>
+   
     public partial class ServicePage : Page
     {
+        int CountRecords;
+        int CountPage;
+        int CurrentPage = 0;
+        List<Service> CurrentPageList= new List<Service>();
+        List<Service> TableList;
         public ServicePage()
         {
             InitializeComponent();
@@ -127,7 +133,132 @@ namespace AyupovAutoService
         private void DeleteButton_Click(object sender, RoutedEventArgs e)
         {
             var currentService = (sender as Button).DataContext as Service;
-            if(MessageBox.Show("Вы точно хотите выполнить удаление?"))
+
+            var currentClientServices = Ayupov_ServiceEntities.GetContext().ClientService.ToList();
+            currentClientServices = currentClientServices.Where(p => p.ServiceID == currentService.ID).ToList();
+
+            if (currentClientServices.Count != 0)
+                MessageBox.Show("Невозможно выполнить удаление, так как существуют записи на эту услугу");
+            else
+            {
+                if (MessageBox.Show("Вы точно хотите выполнить удаление?", "Внимание!",
+                    MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+                {
+                    try
+                    {
+                        Ayupov_ServiceEntities.GetContext().Service.Remove(currentService);
+                        Ayupov_ServiceEntities.GetContext().SaveChanges();
+
+                        ServiceListView.ItemsSource = Ayupov_ServiceEntities.GetContext().Service.ToList();
+                        UpdateServices();
+
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message.ToString());
+                    }
+                }
+            }
+        }
+
+        private void LeftDirButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RightDirButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void ChangePage(int direction, int? selectedPage)
+        {
+            CurrentPageList.Clear();
+            CountRecords=TableList.Count;
+
+            if(CountRecords %10>0)
+            {
+                CountPage = CountRecords / 10 + 1;
+
+            }
+            else 
+            {
+                CountPage = CountRecords / 10;
+            }
+
+            Boolean Ifupdate = true;
+
+            int min;
+
+            if(selectedPage.HasValue) 
+            {
+                if(selectedPage>=0 && selectedPage<=CountPage) 
+                {
+                    CurrentPage=(int)selectedPage;
+                    min = CurrentPage * 10 + 10 < CountRecords ? CurrentPage * 10 + 10 : CountRecords;
+                    for(int i = CurrentPage*10;i<min;i++)
+                    {
+                        CurrentPageList.Add(TableList[i]);
+
+                    }
+                }
+                else
+                {
+                    switch(direction)
+                    {
+                        case 1:
+                            if(CurrentPage>0)
+                            {
+                                CurrentPage--;
+                                min=CurrentPage*10+10<CountRecords ? CurrentPage*10+10 : CountRecords;
+                                for( int i = CurrentPage*10;i<min;i++)
+                                {
+                                    CurrentPageList.Add(TableList[i]);
+                                }
+                            }
+                            else
+                            {
+                                Ifupdate = false;
+                            }
+                            break;
+
+                        case 2:
+                            if(CurrentPage<CountPage-1)
+                            {
+                                CurrentPage++;
+                                min = CurrentPage * 10 + 10 < CountRecords ? CurrentPage * 10 + 10 : CountRecords;
+                                for(int i=CurrentPage*10;i<min;i++)
+                                {
+                                    CurrentPageList.Add(TableList[i]);
+
+                                }
+                                
+                            }
+                            else
+                            {
+                                Ifupdate = false;
+                            }
+                            break;
+                            if (Ifupdate)
+                            {
+                                PageListBox.Items.Clear();
+                                for(int i = 1;i<=CountPage;i++) 
+                                {
+                                    PageListBox.Items.Add(i);
+                                }
+                                PageListBox.SelectedIndex = CurrentPage;
+                                ServiceListView.ItemsSource = CurrentPageList;
+
+                                ServiceListView.Items.Refresh(); 
+                            }
+                        
+                    }
+                }
+            }
+        }
+
+        private void PageListBox_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            ChangePage(0, Convert.ToInt32(PageListBox.SelectedItem.ToString())-1);
         }
     }
 }
