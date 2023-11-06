2c4671778003479b261b479a1a0e45ddd46fb5d1
Ayupov Bulat <b.aiupov@yandex.ru>
2023-11-06 14:36:33 +0500

    8 ЛАБА ГТОВАЯ

--- AyupovAutoService/AddEditPage.xaml.cs
+++ AyupovAutoService/AddEditPage.xaml.cs
@@ -22,74 +22,68 @@ namespace AyupovAutoService
     public partial class AddEditPage : Page
     {
         private Service _currentServise = new Service();
+        public bool IsServiceExist=false;
+       
         public AddEditPage(Service SelectedService)
         {
             InitializeComponent();
             if (SelectedService != null)
+            {
                 _currentServise = SelectedService;
-
+                IsServiceExist=true;    
+            }
             DataContext = _currentServise;
         }
-
+        
         private void SaveButton_Click(object sender, RoutedEventArgs e)
         {
-            StringBuilder errors = new StringBuilder();
+            var errors = new StringBuilder();
+            if (string.IsNullOrWhiteSpace(_currentServise.Title)) errors.AppendLine("Укажите название услуги");
+            if (_currentServise.Cost == 0) errors.AppendLine("Укажите стоимость услуги");
 
-            if (string.IsNullOrWhiteSpace(_currentServise.Title))
-                errors.AppendLine("Укажите название услуги");
-            if (_currentServise.Cost == 0)
-                errors.AppendLine("Укажите стоимость услуги");
-
-            if (_currentServise.Discount<0)
+            if (_currentServise.Discount < 0)
                 errors.AppendLine("Укажите скидку");
-            if(string.IsNullOrWhiteSpace(_currentServise.Discount.ToString()))
-            {
+            if (_currentServise.Discount > 100)
+                errors.AppendLine("Невозможно указать такую скидку");
+
+            if (string.IsNullOrWhiteSpace(_currentServise.Discount.ToString()))
                 _currentServise.Discount = 0;
+            if (_currentServise.DurationInSeconds < 0 || _currentServise.DurationInSeconds > 240)
+                errors.AppendLine("Невозможно указать такую длительность");
+            if (string.IsNullOrWhiteSpace(_currentServise.DurationInSeconds.ToString()))
+                _currentServise.DurationInSeconds = 0;
 
-            }
-
-               
-            if (_currentServise.DurationInSeconds==0)
-                errors.AppendLine("Укажите длительность услуги");
-
-            if(_currentServise.DurationInSeconds>240 || _currentServise.DurationInSeconds <= 0) 
-            {
-                errors.AppendLine("Длительность не может быть больше 240 минут или меньше 0");
-
-            }
-           
-
-            if(errors.Length>0)
+            if (errors.Length > 0)
             {
                 MessageBox.Show(errors.ToString());
                 return;
             }
+
             var allServices = Ayupov_ServiceEntities.GetContext().Service.ToList();
-            allServices= allServices.Where(p=>p.Title == _currentServise.Title).ToList();
-            if (allServices.Count == 0)
+            allServices = allServices.Where(p => p.Title == _currentServise.Title).ToList();
+            if (allServices.Count == 0 || IsServiceExist == true)
             {
-
-
                 if (_currentServise.ID == 0)
+                {
                     Ayupov_ServiceEntities.GetContext().Service.Add(_currentServise);
+                }
+
                 try
                 {
                     Ayupov_ServiceEntities.GetContext().SaveChanges();
                     MessageBox.Show("Информация сохранена");
                     Manager.MainFrame.GoBack();
-
                 }
-                catch (Exception ex)
+                catch (Exception exception)
                 {
-                    MessageBox.Show(ex.Message.ToString());
-
+                    MessageBox.Show(exception.Message);
                 }
             }
             else
             {
+                Ayupov_ServiceEntities.GetContext().SaveChanges();
                 MessageBox.Show("Уже существует такая услуга");
             }
-
         }
     }
 }
